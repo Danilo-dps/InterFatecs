36e892f09645e9dc1221c3397b28c6c1707bea81
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-04 21:34:12 -0300

    Switch dentro do while para determinar o turno de estudos

---
+++ ExercicioDecisao10/ExercicioDecisao10/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Qual periodo você estuda? ");
+Console.WriteLine("m - manhã, t - tarde, n - noite");
+Console.WriteLine("Para finalizar digite 0");
+
+char escolha = Console.ReadLine()[0];
+while (escolha != '0') {
+    switch (escolha) {
+        case 'm':
+            Console.WriteLine("Manhã!");
+            break;
+        case 't':
+            Console.WriteLine("Tarde!");
+            break;
+        case 'n':
+            Console.WriteLine("Noite!");
+            break;
+        default:
+            Console.WriteLine("Escolha inválida!");
+            break;
+    }
+    escolha = Console.ReadLine()[0];
+}
+
+Console.WriteLine("Programa finalizado.");
+
+ 
+
+
