de2e6aee96be3f333aeedc0c562ac84c83569476
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-05 11:05:23 -0300

    Usando while e if-else e o break

---
+++ EstruturaRepeticao2/EstruturaRepeticao2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+internal class Program {
+    private static void Main(string[] args) {
+        Console.WriteLine("Informe o nome e senha");
+
+        while (true) {
+            string nome = Console.ReadLine();
+            string senha = Console.ReadLine();
+
+            if (nome == senha) {
+                Console.WriteLine("A senha não pode ser o seu nome!");
+                Console.WriteLine("Informe nome e senha válidos!");
+            } 
+            else {
+                Console.WriteLine("Login efetuado!");
+                break;
+            }
+        }
+    }
+}
