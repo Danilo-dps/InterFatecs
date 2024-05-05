9f7e5c2e91270d41567d78a25ef4f44f971a364e
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-04 21:31:49 -0300

    Usando vetor, while e if-else para determinar se é vogal ou não

---
+++ ExercicioDecisao4/ExercicioDecisao4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite as letra: ");
+
+string[] alfabeto = Console.ReadLine().Split(' '); 
+
+string[] tipos = new string[alfabeto.Length];
+
+for (int i = 0; i < alfabeto.Length; i++) {
+    tipos[i] = alfabeto[i];
+}
+
+foreach (string s in tipos) {
+    if(s == "a" || s == "e" || s == "i" || s == "o" || s == "u") {
+        Console.WriteLine($"{s} é vogal");
+    }
+    else {
+        Console.WriteLine($"{s} é consoante");
+    }
+}
