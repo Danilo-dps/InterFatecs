3a465fde234574b2bb8106c66e7ba74a0e89a101
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-05 11:08:55 -0300

    Usando while, casting e a incrementação

---
+++ EstruturaRepeticao4/EstruturaRepeticao4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+double popA = 80000;
+double popB = 200000;
+double taxA = 0.03;
+double taxB = 0.015;
+int anos = 0;
+
+while (popA < popB) {
+    popA += popA * taxA;
+    popB += popB * taxB;
+    anos++;
+}
+
+Console.WriteLine($"População A: {(int)popA}");
+Console.WriteLine($"População B: {(int)popB}");
+
+Console.WriteLine($"São necessários {anos} anos para A ultrapassar B");
+
