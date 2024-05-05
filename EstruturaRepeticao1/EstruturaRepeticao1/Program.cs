47799ebcfd585dbc8051adb8313c2cf58f5b4786
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-05 11:03:07 -0300

    Usando while e if-else

---
+++ EstruturaRepeticao1/EstruturaRepeticao1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Informe um número entre 0 e 10");
+
+
+while (true) {
+    int num = int.Parse(Console.ReadLine());
+    if (num < 0 || num > 10) {
+        Console.WriteLine("Informe um número válido!");
+    }
+    else {
+        Console.WriteLine("Valor válido!");
+    }
+}
