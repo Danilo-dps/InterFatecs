d2d183e0d89f75c2879a750d717943b774b1dffb
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-04 21:29:01 -0300

    Usando vetor e a função split

---
+++ ExercicioSequencial4/ExercicioSequencial4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.Write("Digite as notas: ");
+
+string[] notas = Console.ReadLine().Split(' ');
+
+long[] longs = new long[notas.Length];
+long count = 0;
+
+for (int i = 0; i < notas.Length; i++) {
+    longs[i] = long.Parse(notas[i]);
+    count += longs[i];
+}
+
+long avg = count / longs.Length;
+
+Console.WriteLine($"A média das notas são {avg}");
