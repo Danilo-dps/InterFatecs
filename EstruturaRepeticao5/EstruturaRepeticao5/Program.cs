354293d8a02053f25585d6cea3b8c697805071d2
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-06 20:50:13 -0300

    Usando While, if-else para cálculo de projeção populacional

---
+++ EstruturaRepeticao5/EstruturaRepeticao5/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Entre com os números populacional.");
+double popA = double.Parse(Console.ReadLine());
+double popB = double.Parse(Console.ReadLine());
+Console.WriteLine("Entre com a taxa de crescimento.");
+double taxA = double.Parse(Console.ReadLine());
+double taxB = double.Parse(Console.ReadLine());
+int anos = 0;
+
+if (popA > popB) {
+    while (popA > popB) {
+        popA += popA * taxA / 100;
+        popB += popB * taxB / 100;
+        anos++;
+    }
+    Console.WriteLine($"População A: {(int)popA}");
+    Console.WriteLine($"População B: {(int)popB}");
+    Console.WriteLine($"São necessários {anos} anos para B ultrapassar A");
+}
+else {
+    while (popA < popB) {
+        popA += popA * taxA / 100;
+        popB += popB * taxB / 100;
+        anos++;
+    }
+    Console.WriteLine($"População A: {(int)popA}");
+    Console.WriteLine($"População B: {(int)popB}");
+    Console.WriteLine($"São necessários {anos} anos para A ultrapassar B");
+}
