cc37167a5277ea1997f4fe7ce737741193ebaeb6
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-05-06 21:08:42 -0300

    Exibindo o maior número em uma sequência aleatória

---
+++ EstruturaRepeticao7/EstruturaRepeticao7/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Digite os números.");
+string[] nums = Console.ReadLine().Split(' ');
+int[] num = new int[nums.Length];
+for (int i = 0; i < nums.Length; i++) {
+    num[i] = int.Parse(nums[i]);
+}
+
+int maior = num[0];
+for (int i = 0; i < nums.Length; i++) {
+    if (num[i] > maior) {
+        maior = num[i];
+    }
+}
+
+Console.WriteLine($"O maior é: {maior}");
